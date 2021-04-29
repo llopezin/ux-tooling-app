be1b39aa4744c0308636db995b07f7d021a1e4f3
Laura Lopez <lauralopezinfantes@gmail.com>
2021-04-29 19:03:30 +0100

    change chart functionality initial implemetation

--- src/app/tasks/results/results.component.ts
+++ src/app/tasks/results/results.component.ts
@@ -4,6 +4,7 @@ import { Task } from 'src/app/campaign/models/task.model';
 import { CompleteTaskService } from '../complete-task/services/complete-task.service';
 import { Chart, registerables } from 'chart.js';
 import { ThrowStmt } from '@angular/compiler';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-results',
@@ -15,31 +16,39 @@ export class ResultsComponent implements OnInit {
   public task: Task;
   public task_id: string;
   public charts: Chart[];
+  public chartTypes = ['bar', 'pie', 'line', 'doughnut', 'polarArea'];
+  public changeChartSelector: FormGroup;
 
-  constructor(private taskService: CompleteTaskService, private route: ActivatedRoute) {
+  constructor(private taskService: CompleteTaskService, private route: ActivatedRoute, private fb: FormBuilder) {
     this.task_id = this.route.snapshot.paramMap.get('id')
   }
 
   ngOnInit(): void {
     this.getTask()
+    this.buildChangeChartForm()
   }
 
   getTask() {
     this.taskService.getTask(this.task_id).subscribe(res => { this.task = res; setTimeout(() => { this.createCharts() }, 500) })
   }
 
+  buildChangeChartForm(){
+    this.changeChartSelector = this.fb.group({chartType: ['bar']})
+  }
+
   createCharts() {
     Chart.register(...registerables);
-    this.task.questions.forEach((question, i) => { this.buildChart(i, question) })
+    this.charts = this.task.questions.map((question, i) => { return this.buildChart(i, question) })
+    console.log('this.charts:', this.charts)
 
   }
 
-  buildChart(i, question) {
+  buildChart(i, question, type: any = 'bar') {
     const id = `question${i}`
     const labels = question.options ? question.options.split(',') : question.tags.split(',')
 
-    new Chart(id, {
-      type: 'bar',
+    let chart = new Chart(id, {
+      type: type,
       data: {
         labels: labels,
         datasets: [{
@@ -56,28 +65,38 @@ export class ResultsComponent implements OnInit {
         },
       },
     });
-    
+
+    return chart
+
   }
 
-  getData(i){
+
+  getData(i) {
     console.log(this.task.questions[i].question)
-    
+
     let question = this.task.questions[i]
     let questionText = question.question
     let options = question.options ? question.options.split(',') : question.tags.split(',')
-    let responses =  this.task.responses.map(response=>response[questionText]).filter(r=>r).flat()
+    let responses = this.task.responses.map(response => response[questionText]).filter(r => r).flat()
     let data = []
-    
+
     options.forEach(option => {
-      data.push(responses.filter(response=>response === option).length)
+      data.push(responses.filter(response => response === option).length)
     });
-    
+
     return data
   }
 
-  getBackgrounds(labels){
+  changeChart(i) {
+    let chart = this.charts[i]
+    chart.config.type = this.changeChartSelector.value.chartType || 'bar'
+    chart.update()
+    console.log('chart:', chart)
+  }
+
+  getBackgrounds(labels) {
     let backgrounds = [];
-    
+
     labels.forEach(label => {
       backgrounds.push(this.random_rgba())
     });
--- src/app/tasks/results/results.module.ts
+++ src/app/tasks/results/results.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ResultsComponent } from './results.component';
 import { ResultsRoutingModule } from './results-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
 
 @NgModule({
   declarations: [ResultsComponent],
   imports: [
-    CommonModule, ResultsRoutingModule
+    CommonModule, ResultsRoutingModule, ReactiveFormsModule, FormsModule
   ]
 })
 export class ResultsModule { }
